099411c9f5b9aab822a07e6230e21720bfc09e73
Himanshu kumar <himanshukr1505892@gmail.com>
2025-03-13 23:32:19 +0530

    first commmit

--- server/config/connectToDB.js
+++ server/config/connectToDB.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectToDB = () => {
   mongoose
-    .connect('mongodb+srv://ravikant59826:Himanshu123123@cluster0.ui5ry.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+    .connect('mongodb+srv://himanshukr1505892:6cnCTRXkuMAKHlTu@cluster0.ui5ry.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
