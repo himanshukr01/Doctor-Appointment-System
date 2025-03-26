9ff958be7d44a5591762569fec855dee5f8585be
Himanshu kumar <himanshukr1505892@gmail.com>
2025-03-26 22:38:22 +0530

    first commit

--- server/config/connectToDB.js
+++ server/config/connectToDB.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 
 const connectToDB = () => {
   mongoose
-    .connect('mongodb+srv://himanshukr1505892:6cnCTRXkuMAKHlTu@cluster0.imnoc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(() => {
-      console.log('Connected to MongoDB Atlas');
-    })
+    // .connect('mongodb+srv://himanshukr1505892:6cnCTRXkuMAKHlTu@cluster0.imnoc.mongodb.net/', {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    // })
+    // .then(() => {
+    //   console.log('Connected to MongoDB Atlas');
+    // })
     .catch((err) => {
       throw new Error(`Could not connect to MongoDB: ${err}`);
     });
