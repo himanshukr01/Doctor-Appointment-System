b6224c948d0b9da5a3276a6f1972c8ba3a60789f
Himanshu kumar <himanshukr1505892@gmail.com>
2025-04-05 22:04:12 +0530

    first commit

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,17 +1,17 @@
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-//   theme: {
-//     extend: {
-//       fontFamily: {
-//         inter: ['Inter', 'sans-serif'], // Custom keyword for Inter
-//         viga: ['Viga', 'sans-serif'],   // Custom keyword for Viga
-//       },
-//     },
-//   },
-//   plugins: {
-//     tailwindcss: {},
-//     autoprefixer: {},
-//   },
-// };
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'], // Custom keyword for Inter
+        viga: ['Viga', 'sans-serif'],   // Custom keyword for Viga
+      },
+    },
+  },
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
 
