8179cc06d683364040fb4f3284e68d2d7c71fb21
Himanshu kumar <himanshukr1505892@gmail.com>
2025-04-07 22:07:44 +0530

    second commit

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,17 +1,17 @@
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-//   theme: {
-//     extend: {
-//       fontFamily: {
-//         inter: ['Inter', 'sans-serif'], 
-//         viga: ['Viga', 'sans-serif'],   
-//       },
-//     },
-//   },
-//   plugins: [
-//     require('@tailwindcss/postcss'),
-//     require('autoprefixer'),
-// ],
-// };
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'], 
+        viga: ['Viga', 'sans-serif'],   
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/postcss'),
+    require('autoprefixer'),
+],
+};
 
